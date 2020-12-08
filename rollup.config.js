b008ef05bc34a70220b6876d91baee24e7230efc
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2020-12-08 17:30:33 +0300

    Update exported filename

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import autoExternal from 'rollup-plugin-auto-external'
 
-export default {
-  input: {
-    environment: 'src/environment/index.js'
-  },
-  output: {
-    dir: 'dist',
-    format: 'es'
-  },
-  plugins: [
-    autoExternal()
-  ]
-}
+export default [
+  {
+    input: 'src/environment/index.js',
+    output: {
+      file: 'environment.js',
+      format: 'es'
+    },
+    plugins: [
+      autoExternal()
+    ]
+  }
+]
